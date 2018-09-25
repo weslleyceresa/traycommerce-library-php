97c2b6d71e728c6acfffd837caf33578a397e81c
Weslley Ceresa <weslley_ceresa@hotmail.com>
2018-09-25 11:54:00 -0300

    .

--- src/Helpers.php
+++ src/Helpers.php
@@ -1,6 +1,6 @@
 <?php 
 	function generateLog($text){
-		$file = fopen("../logs/logs.txt", "a+");
+		$file = fopen(__DIR__ . "../logs/logs.txt", "a+");
 		fwrite($file, "[".date("d/m/Y H:i:s")."] - ".$text."\n");
 		fclose($file);
 	}
