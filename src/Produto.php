84de41268c8eab6e1a7b608bcdfe5e41dc4d28fe
Weslley Ceresa <weslley_ceresa@hotmail.com>
2020-06-24 16:58:55 -0300

    Corrigido metodo de cadastro de produto

--- src/Produto.php
+++ src/Produto.php
@@ -105,7 +105,7 @@ class Produto extends BaseEndpoints{
             "access_token" => $this->trayCommerceController->getToken()->getAccess_token()
         );
         
-        $resposta = $this->put(self::uri, $data, $query);
+        $resposta = $this->post(self::uri, $data, $query);
 
         if (GlobalHelper::success($resposta["code"])) {
             return $resposta["data"];
